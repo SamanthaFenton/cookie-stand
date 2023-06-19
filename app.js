5886e035308a3ba50e9a99346fa318e89e250724
Samantha <samantha.fenton@live.co.uk>
2023-06-19 15:22:54 +0100

    linking dom and javaScript

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
 "use strict"
-console.log(salmoncookie)
+console.log("salmoncookies")
+
+const container = document.getElementsById("container");
+
+const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm","6pm", "7pm", "8pm"];
+console.log(hours.length);
 
 const shop = {
-    location: seattle,
+    storeName: "seattle",
     minCustomerPerHr: 23,
     maxCustomerPerHr: 65,
     avgCustomerPerHr: 0,
     avgCookiePerCustomer: 6.3,
     hoursOfService: 14,
-    totalDailyCookies: 0;
+    avgCookiesPerHour: 0,
+    totalDailyCookies: 0,
+    render: function () {
+        const article = document.createElement("article");
+        container.appendChild(article);
 
-    customersPerHr: function () {
+        const h3 = document.createElement("h3");
+        h3.textContent = this.storeName;
+        article.appendChild(article);
 
-        this.avgCustomerPerHr = randomCustomersPerHr(23,65) + "customers per hour";
-        console.log(this.avgCustomerPerHr);
-    }
-}
+        const ul = document.createElement("ul");
+        article.appendChild(ul);
 
-function randomCustomersPerHr(minCustomer, maxCustomer) {
-    return Math.floor(Math.random() * (maxCustomer - minCustomer +1) + minCustomer);
+        for (let i = 0; i < hours.length; i++) {
+            const li = document.createElement("li");
+            li.textContent = $hours[i]; $this.cookiesEachHour[i];}
+            ul.appendChild(li);
+            }
+        }
+    //customersPerHr: function () {
 
-}
-
-function multiply(hoursOfService, avgCookiePerCustomer, avgCustomerPerHr) {
+       // this.avgCustomerPerHr = randomCustomersPerHr(23,65) + "customers per hour";
+        //console.log(this.avgCustomerPerHr);
     
-}
+
+
+//function randomCustomersPerHr(minCustomer, maxCustomer) {
+    //return Math.floor(Math.random() * (maxCustomer - minCustomer +1) + minCustomer);
+
+
+
+//function multiply(hoursOfService, avgCookiePerCustomer, avgCustomerPerHr){
+  
+
+    
+
 
 
 
